eb5bc71ad21e70c642095997628affe950f57ea0
3485mm <93399106+3485mm@users.noreply.github.com>
2023-12-05 16:50:59 +0400

    1)

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.init()
+screen = pygame.display.set_mode((1280, 650))
+screen.fill((0, 0, 0))
+pygame.display.set_caption("Game")
+pygame.draw.rect(screen, (237, 145, 47), (13, 12, 462, 23))  # оранжевый квадрат слева
+a = pygame.draw.rect(screen, (224, 56, 73), (13, 42, 462, 183), 2)  # красная сетка слева
+pygame.draw.line(screen, (224, 56, 73), [121, 42], [121, 225], 2)
+pygame.draw.line(screen, (224, 56, 73), [242, 42], [242, 225], 2)
+pygame.draw.line(screen, (224, 56, 73), [363, 42], [363, 225], 2)
+pygame.draw.rect(screen, (137, 224, 56), (13, 228, 462, 181), 2)  # зелёная сетка слева
+pygame.draw.line(screen, (137, 224, 56), [121, 228], [121, 408], 2)
+pygame.draw.line(screen, (137, 224, 56), [242, 228], [242, 408], 2)
+pygame.draw.line(screen, (137, 224, 56), [363, 228], [363, 408], 2)
+b = int(input())
+if b == 0:
+    a = 0
+clock = pygame.time.Clock()
+pygame.display.update()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
